65831d9b9d021d9255df4d51c3ec25635d994d73
rasanashr <rasanashr@gmail.com>
2026-06-04 21:42:23 +0330

    00

--- src/routes/api/auth/logout/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export async function POST({ cookies }) {
-  cookies.delete('token', { path: '/' });
-  return json({ message: 'Logged out successfully' }, { status: 200 });
-}
